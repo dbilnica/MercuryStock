c1870fbc64b7f17f09e4c8e809c7ce0e1faf3002
David Bilnica <bilnica@cngroup.dk>
2023-02-07 20:28:15 +0100

    Added CompanyListings States

---
+++ app/src/main/java/com/example/mercurystock/presentation/company_listings/CompanyItem.kt
@@ -0,0 +1,2 @@
+package com.example.mercurystock.presentation.company_listings
+
---
+++ app/src/main/java/com/example/mercurystock/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.example.mercurystock.presentation.company_listings
+
+import com.example.mercurystock.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
